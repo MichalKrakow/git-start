023af911985e6c4d952fe870d91ce4503cb80f69
michal <michal@ikropka.com>
2017-03-19 19:41:36 +0100

    Project start

---
+++ back/SomeClass.php
@@ -0,0 +1,11 @@
+<?php
+
+
+class ClassName extends AnotherClass
+{
+	
+	function __construct(argument)
+	{
+
+	}
+}
---
+++ front/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>This is some html starter</title>
+</head>
+<body>
+
+</body>
+</html>
