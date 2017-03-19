2bf8b7c986db7a81970566ded9f76d4bcc960d5a
michal <michal@ikropka.com>
2017-03-19 20:01:50 +0100

    made some gr8 front end stuff

---
+++ front/body.php
@@ -0,0 +1,5 @@
+<!-- and i would extract a body because: fuck you -->
+<div>
+	<h1>Hey let's have some vc</h1>
+	<p>This is going to suck</p>
+</div>
--- front/index.php
+++ front/index.php
@@ -4,9 +4,6 @@
 	<title>This is some html starter</title>
 </head>
 <body>
-	<div>
-		<h1>Hey let's have some vc</h1>
-		<p>This is going to suck</p>
-	</div>
+	
 </body>
 </html>
